1317872ae606a0f28cd7c4d41733ca8c25742908
Shpyro Philip <philip.shpyro@gmail.com>
2019-09-23 00:22:06 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string word = Console.ReadLine();
+                int count = 0, maxCount = 0;
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (word[i] == word[i + 1])
+                    {
+                        count++;
+                        if (count > maxCount)
+                            maxCount = count;
+                    }
+                    else
+                        count = 0;
+                }
+                Console.WriteLine(++maxCount);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
