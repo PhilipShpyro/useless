abb93e12eceec461b7e5db9d7010006c3d6e3bd9
PhilipShpyro <55670267+PhilipShpyro@users.noreply.github.com>
2019-11-22 02:44:28 +0300

    Create Program.cs

---
+++ DEV_3/DEV_3/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DEV_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sequence sequence = new Sequence("deadline");
+            //Console.WriteLine(sequence.GetChangedString());
+        }
+    }
+}
