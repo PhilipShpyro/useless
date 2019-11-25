2298d2011c7c060c88d6d639ddde38ccd44af3c6
PhilipShpyro <55670267+PhilipShpyro@users.noreply.github.com>
2019-11-25 13:06:34 +0300

    Update Program.cs

--- DEV_4/DEV_4/Program.cs
+++ DEV_4/DEV_4/Program.cs
@@ -7,8 +7,13 @@ namespace DEV_4
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.mail.ru";
+            IWebDriver driver = new ChromeDriver(@"C:\Users\Philipp\source\repos\DEV_4\DEV_4\bin\Debug\netcoreapp2.1");
+            driver.Navigate().GoToUrl("https://mail.ru");
+            IWebElement loginField = driver.FindElement(By.Name("login"));
+            loginField.SendKeys(LOGIN + Keys.Enter);
+
+            IWebElement passwordField = driver.FindElement(By.Name("password"));
+            loginField.SendKeys(PASSWORD + Keys.Enter);
             driver.Quit();
         }
     }
