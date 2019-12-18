f869263b609cfd9df67adfe83061f0b2053670fd
PhilipShpyro <55670267+PhilipShpyro@users.noreply.github.com>
2019-12-18 18:07:46 +0300

    Add files via upload

---
+++ DEV_7/DEV_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace DEV_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalkulyatorPage page = new CalkulyatorPage();
+            page.GoOn();
+            Console.WriteLine(page.GetAfterDotLength());
+            page.ChangeAfterDotLength(5);
+            Console.WriteLine(page.GetAfterDotLength());
+        }
+    }
+}
